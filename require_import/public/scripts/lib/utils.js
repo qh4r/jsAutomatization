54d03c32afc0a62dd8190406f3a1605f8f6926cc
Rafa³ Kucharski <r.kucharski@lgbs.pl>
2016-09-24 18:42:19 +0200

    Fun with es2015 import syntax

---
+++ require_import/public/scripts/lib/utils.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.test1 = test1;
+exports.test2 = test2;
+function test1() {
+    console.log('111');
+}
+
+function test2() {
+    console.log('222');
+}
---
+++ require_import/src/scripts/lib/utils.js
@@ -0,0 +1,7 @@
+export function test1(){
+    console.log('111');
+}
+
+export function test2(){
+    console.log('222');
+}
